38c466e507b51b54920f3129640630ac34c09cfa
kad0602 <Kseniia.Dubovyk@infiz.khpi.edu.ua>
2026-05-19 17:08:45 +0200

    Enhance CI/CD pipeline and update main endpoint response for Google Cloud Run deployment

--- main.go
+++ main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Створюємо базовий роутер Gin
 	r := gin.Default()
 
-	// Налаштовуємо простий ендпоінт
 	r.GET("/", func(c *gin.Context) {
+		// Зчитуємо секрет, який Cloud Run передасть у змінну оточення
+		secretKey := os.Getenv("MY_API_KEY")
+		if secretKey == "" {
+			secretKey = "Секрет локальний або не знайдено!"
+		}
+
 		c.JSON(http.StatusOK, gin.H{
 			"status":  "success",
-			"message": "Hello World from Go Gin API!",
+			"message": "Hello World from Google Cloud Run!",
+			"secret":  secretKey, // Виводимо для демонстрації викладачу
 		})
 	})
 
-	// Запускаємо сервер ТА перевіряємо наявність помилок
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("Помилка при запуску сервера: %v", err)
 	}
